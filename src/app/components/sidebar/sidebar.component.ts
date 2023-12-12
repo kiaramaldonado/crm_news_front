5a3c229032b06b9b9062cefef1d5a04cfcab6395
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2023-12-12 12:24:53 +0000

    Sidebar with categories to be shown in Home page.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Category } from 'src/app/core/models/category.interface';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  @Input() categories: Category[] = [];
+  @Input() selectedCategory: Category | null = null;
+  @Output() categorySelected = new EventEmitter<Category | null>();
 
+  emitCategorySelection(category: Category | null): void {
+    this.selectedCategory = category;
+    this.categorySelected.emit(category);
+  }
 }
