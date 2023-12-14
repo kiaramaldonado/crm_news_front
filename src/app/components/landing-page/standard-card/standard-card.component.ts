9da8b2bbdb52a4471454671f17251ee642555036
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2023-12-14 11:22:07 +0000

    Sidebar turned into navbar.

--- src/app/components/landing-page/standard-card/standard-card.component.ts
+++ src/app/components/landing-page/standard-card/standard-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Article } from 'src/app/core/models/article.interface';
+import { Category } from 'src/app/core/models/category.interface';
 
 @Component({
   selector: 'app-standard-card',
@@ -7,10 +8,25 @@ import { Article } from 'src/app/core/models/article.interface';
   styleUrls: ['./standard-card.component.css']
 })
 export class StandardCardComponent {
-  @Input() article: Pick<Article, 'title' | 'excerpt' | 'url'> = {
+  @Input() article: Pick<Article, 'title' | 'excerpt' | 'url' | 'category_id'> = {
     title: '',
     excerpt: '',
     url: '',
+    category_id: 1,
   };
 
+  categories: Category[] = [];
+
+  getCategoryRoute(categoryId: number): string {
+    const category = this.categories.find(c => c.id === categoryId);
+    if (category) {
+      return category.name.toLowerCase().replace(/[,\s]+/g, '-');
+    } else {
+      return '';
+    }
+  }
+
+  transformTitle(title: string): string {
+    return title.toLowerCase().replace(/[,\s]+/g, '-');
+  }
 }
