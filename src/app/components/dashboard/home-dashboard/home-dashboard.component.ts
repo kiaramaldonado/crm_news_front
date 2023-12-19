586dc30215ae378193d146d51c78e536bd73fec3
Claudia Turro Pujol <cturropujol@gmail.com>
2023-12-19 11:09:05 +0100

    New message for user without articles assigned.

--- src/app/components/dashboard/home-dashboard/home-dashboard.component.ts
+++ src/app/components/dashboard/home-dashboard/home-dashboard.component.ts
@@ -35,6 +35,30 @@ export class HomeDashboardComponent {
     }
   }
 
+//   async ngOnInit() {
+//   try {
+//     this.user = await this.usersService.getById();
+//     console.log(this.user);
+//     this.articlesArr = await this.articlesService.getByUser();
+
+//     // Verificar si hay elementos en this.articlesArr
+//     if (this.articlesArr.length > 0) {
+//       this.articlesArr = (await this.filtrarStatus()).reverse();
+//       console.log(this.articlesArr);
+//       const newFiltrado = [...new Map(this.articlesArr.map((item: { id: number; }) => [item.id, item])).values()];
+//       console.log(newFiltrado);
+//       this.lastAsign = newFiltrado.slice(0, 2); // Obtener los dos primeros elementos
+//       console.log(this.lastAsign);
+//     } else {
+//       console.log("El usuario no tiene asignaciones.");
+//       // Puedes manejar este caso según tus necesidades, por ejemplo, mostrar un mensaje al usuario.
+//     }
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
   async filtrarStatus() {
     if (this.user && this.user.role === 'editor') {
       return this.articlesArr.filter(article => article.status === "revision");
